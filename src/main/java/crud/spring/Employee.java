7e3dab62e8f4885d94acffaff99c4b5eee798abb
Xiao Lu <xiaolu@Xiaos-MacBook-Pro.local>
2019-10-18 17:39:56 -0700

    first commit

---
+++ src/main/java/crud/spring/Employee.java
@@ -0,0 +1,14 @@
+package crud.spring;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+    private String employNumber;
+    private String employName;
+    private String position;
+}
